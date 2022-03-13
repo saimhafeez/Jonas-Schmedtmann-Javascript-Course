ed078798c1b9b4eb2bd29dc70481920b3f3f33f5
saimhafeez2020 <saimhafeez2020@gmail.com>
2022-03-13 22:59:10 +0500

    Day 1 (2)

--- Code Alongs/01-Fundamentals-Part-1/script.js
+++ Code Alongs/01-Fundamentals-Part-1/script.js
@@ -1,4 +1,21 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
+// if (js === 'amazing') alert('Javascript is FUN!');
 
 console.log(40 - 10 + 30);
+
+let age = 30;
+age = 31;
+
+const birthYear = 2001;
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
+
+const firstName = 'saim';
+const lastName = 'hafeez';
+
+console.log(firstName + ' ' + lastName);
