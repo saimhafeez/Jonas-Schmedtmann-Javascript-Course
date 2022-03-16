6a6d95083060e34918f45e0e4a1312ea6bf0e812
saimhafeez2020 <saimhafeez2020@gmail.com>
2022-03-16 07:49:20 +0500

    Day 4 (2)

--- Code Alongs/02-Fundamentals-Part-2/scripts.js
+++ Code Alongs/02-Fundamentals-Part-2/scripts.js
@@ -11,3 +11,23 @@ const yearsUnitRetirement = (birthYear) => {
 };
 
 console.log(yearsUnitRetirement(1991));
+
+// ARRAYS
+const friends = ['saim', 'arslan', 'mehroz', 'abdullah'];
+const years = new Array(12, 'saim');
+console.log(years, friends, friends.length);
+
+const data = [friends, years];
+console.log(data);
+
+friends.push('awais');
+friends.unshift('Abdl sattar');
+console.log(friends);
+
+friends.pop();
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('arslan'));
+
+console.log(friends.includes('usman'));
