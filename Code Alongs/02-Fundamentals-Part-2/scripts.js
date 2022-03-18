65d0453fc9ba5ecfd2cdcc01ca1d8c85baf46239
saimhafeez2020 <saimhafeez2020@gmail.com>
2022-03-19 00:10:59 +0500

    Day 5 (1)

--- Code Alongs/02-Fundamentals-Part-2/scripts.js
+++ Code Alongs/02-Fundamentals-Part-2/scripts.js
@@ -31,3 +31,24 @@ console.log(friends);
 console.log(friends.indexOf('arslan'));
 
 console.log(friends.includes('usman'));
+
+// OBJECTS
+const jonas = {
+  firstName: 'Johnas',
+  lastName: 'shmedtmann',
+  age: 2036 - 1991,
+  job: 'teacher',
+  friends: ['Micheaal', 'Peter', 'Steven'],
+};
+
+console.log(jonas.firstName);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+jonas.location = 'Pakistan';
+
+jonas['fb'] = '/saimhafeez';
+
+console.log(jonas);
