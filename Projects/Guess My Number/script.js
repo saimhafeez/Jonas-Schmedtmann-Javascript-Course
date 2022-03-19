de69c9206c8cd392ca846b48a90e90b4c0077342
saimhafeez2020 <saimhafeez2020@gmail.com>
2022-03-20 01:53:14 +0500

    Day 6 (2)

--- Projects/Guess My Number/script.js
+++ Projects/Guess My Number/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const getrandom = () => Math.trunc(Math.random() * 20 + 1);
+
 // Elements
 
 const check_btn = document.querySelector('.check');
@@ -8,7 +10,7 @@ const guess_input = document.querySelector('.guess');
 
 const msg = document.querySelector('.message');
 
-const secret_number = Math.trunc(Math.random() * 20 + 1);
+let secret_number = getrandom();
 
 const number = document.querySelector('.number');
 
@@ -30,6 +32,8 @@ check_btn.addEventListener('click', function () {
 
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
+
+      check_btn.disabled = true;
     } else if (Number(guess_input.value) > secret_number) {
       msg.textContent = 'Too High';
       score.textContent = Number(score.textContent) - 1;
@@ -37,5 +41,31 @@ check_btn.addEventListener('click', function () {
       msg.textContent = 'Too Low';
       score.textContent = Number(score.textContent) - 1;
     }
+    if (score.textContent < 1) {
+      msg.textContent = 'You Lost!';
+    }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score.textContent = 20;
+  secret_number = getrandom();
+  number.textContent = secret_number;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  check_btn.disabled = false;
+});
+
+/*
+Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+*/
